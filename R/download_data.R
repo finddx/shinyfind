e40c328c5eaeb9e892a081a958f8bf95ee5d7bbb
Christoph Sax <christoph.sax@gmail.com>
2021-11-15 09:50:34 +0100

    input$i_download_all must be in a reactive expression!

--- R/download_data.R
+++ R/download_data.R
@@ -4,18 +4,15 @@ download_data_server <- function(id, data) {
   
   moduleServer(id, function(input, output, session) {
     
-    data_download <- data
-    type <- input$i_download_all
-    
     output$o_download_all <- downloadHandler(
       filename = function() {
-        paste("data-", Sys.Date(), if (type == "csv") ".csv" else ".xlsx", sep = "")
+        paste("data-", Sys.Date(), if (input$i_download_type == "csv") ".csv" else ".xlsx", sep = "")
       },
       content = function(con) {
-        if (type == "csv") {
-          write_csv(data_download, con)
+        if (input$i_download_type == "csv") {
+          write_csv(data, con)
         } else {
-          writexl::write_xlsx(data_download, con)
+          writexl::write_xlsx(data, con)
         }
       }
     )
@@ -32,7 +29,7 @@ download_data_ui <- function(id = NULL) {
   tags$span(style = "display:flex;", p(
     "All the data can be downloaded ", downloadLink(ns("o_download_all"), label = "here."), tags$span(style = "margin-left: 15px"),
     prettyRadioButtons(
-      inputId = ns("i_download_all"),
+      inputId = ns("i_download_type"),
       label = NULL,
       status = "default",
       inline = T,
