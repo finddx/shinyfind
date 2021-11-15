9891c95aa00fdf841cb964ec65344fd179d120d6
benubah <ben@rpowerlabs.org>
2021-11-15 17:33:26 +0100

    get_meta -> get_codebook

--- R/get_codebook.R
+++ R/get_codebook.R
@@ -2,7 +2,7 @@
 #'
 #' Memoised Function, updates once a day
 #'
-#' @name get_meta
+#' @name get_codebook
 get_codebook_ <- function(time = shinyfind:::today_at_sunrise()) {
   codebook <- readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/FINDCov19TrackerData/master/processed/codebook.csv", col_types = readr::cols())
   codebook
