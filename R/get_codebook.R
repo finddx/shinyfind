d8e3aae5c7a24bded31364511a9771d002be1af2
Christoph Sax <christoph.sax@gmail.com>
2021-11-17 16:24:11 +0100

    access function get_codebook_extended()

--- R/get_codebook.R
+++ R/get_codebook.R
@@ -12,6 +12,15 @@ get_codebook_ <- function(time = shinyfind:::today_at_sunrise()) {
 get_codebook <- memoise::memoise(get_codebook_)
 
 
+#' @export
+#' @name get_codebook
+get_codebook_extended <- function(show_cacluation = FALSE) {
+  ans <- readr::read_csv(system.file(package = "shinyfind", "codebook/codebook_extended.csv"), col_types = "c")
 
+  if (show_cacluation == FALSE) {
+    ans <- dplyr::select(ans, -calculation)
+  }
 
+  ans
+}
 
