1301ff6100720194922a68833fce97a30ae29dde
Christoph Sax <christoph.sax@gmail.com>
2022-01-27 23:55:23 +0100

    two stage summary

--- R/summraize_over_group_and_time_range.R
+++ R/summraize_over_group_and_time_range.R
@@ -13,58 +13,80 @@ summarize_over_group_and_time_range <- function (data_all, country_last_update_i
     filter(set == "country") |>
     select(-name, -set) |>
     left_join(country_last_update_info, by = "unit") |>
-    filter(!is.na(last_update))
-  
-  # if time_range
-  data_table  <-
-    data_filtered |>
+    filter(!is.na(last_update))|>
     filter(dplyr::between(
       time,
-      as.Date(start_date), 
+      as.Date(start_date),
       as.Date(end_date)
     ))
-  
-  if (grouped) {
-    data_table <-
-      data_table |>
-      rename_with(function(x) "group", {{ category }}) |>
-      filter(!is.na(group)) |>
-      mutate(unit = group, name = group)
-  } else {
-    data_table <-
-      data_table |>
-      mutate(group = unit)
-  }
-  
-  data_table <-
-    data_table |>
+# browser()
+  data_summarized <-
+    data_filtered |>
     group_by(unit) |>
     summarize(
       name = name[1],
-      group = group[1],
-      pop_100k               = if(grouped) sum(pop_100k[time == end_date], na.rm = TRUE) else mean(pop_100k, na.rm = TRUE),
-      pop                    = if(grouped) sum(pop[time == end_date], na.rm = TRUE) else mean(pop, na.rm = TRUE),
-      avg_cap_new_cases      = mean(all_new_cases / pop       , na.rm = TRUE),
-      sum_cap_new_cases      = sum(all_new_cases / pop        , na.rm = TRUE),
-      avg_cap_new_deaths     = mean(all_new_deaths / pop      , na.rm = TRUE),
-      sum_cap_new_deaths     = sum(all_new_deaths / pop       , na.rm = TRUE),
-      avg_cap_new_tests      = mean(all_new_tests / pop       , na.rm = TRUE),
-      sum_cap_new_tests      = sum(all_new_tests / pop        , na.rm = TRUE),
-      avg_all_new_cases      = mean(all_new_cases             , na.rm = TRUE),
-      sum_all_new_cases      = sum(all_new_cases              , na.rm = TRUE),
-      avg_all_new_deaths     = mean(all_new_deaths            , na.rm = TRUE),
-      sum_all_new_deaths     = sum(all_new_deaths             , na.rm = TRUE),
-      avg_all_new_tests      = mean(all_new_tests             , na.rm = TRUE),
-      sum_all_new_tests      = sum(all_new_tests              , na.rm = TRUE),
-      avg_pos                = avg_all_new_cases / avg_all_new_tests,
-      avg_cap100k_new_cases  = mean(all_new_cases / pop_100k  , na.rm = TRUE),
-      sum_cap100k_new_cases  = sum(all_new_cases / pop_100k   , na.rm = TRUE),
-      avg_cap100k_new_deaths = mean(all_new_deaths / pop_100k , na.rm = TRUE),
-      sum_cap100k_new_deaths = sum(all_new_deaths / pop_100k  , na.rm = TRUE),
-      avg_cap100k_new_tests  = mean(all_new_tests / pop_100k  , na.rm = TRUE),
-      sum_cap100k_new_tests  = sum(all_new_tests / pop_100k   , na.rm = TRUE),
+      # group = group[1],
+      pop_100k                 = mean(pop_100k, na.rm = TRUE),
+      pop                      = mean(pop, na.rm = TRUE),
+      avg_cap_new_cases        = mean(all_new_cases / pop       , na.rm = TRUE),
+      sum_cap_new_cases        = sum(all_new_cases / pop        , na.rm = TRUE),
+      avg_cap_new_deaths       = mean(all_new_deaths / pop      , na.rm = TRUE),
+      sum_cap_new_deaths       = sum(all_new_deaths / pop       , na.rm = TRUE),
+      avg_cap_new_tests        = mean(all_new_tests / pop       , na.rm = TRUE),
+      sum_cap_new_tests        = sum(all_new_tests / pop        , na.rm = TRUE),
+      avg_all_new_cases        = mean(all_new_cases             , na.rm = TRUE),
+      sum_all_new_cases        = sum(all_new_cases              , na.rm = TRUE),
+      avg_all_new_deaths       = mean(all_new_deaths            , na.rm = TRUE),
+      sum_all_new_deaths       = sum(all_new_deaths             , na.rm = TRUE),
+      avg_all_new_tests        = mean(all_new_tests             , na.rm = TRUE),
+      sum_all_new_tests        = sum(all_new_tests              , na.rm = TRUE),
+      avg_pos                  = avg_all_new_cases / avg_all_new_tests,
+      avg_cap100k_new_cases    = mean(all_new_cases / pop_100k  , na.rm = TRUE),
+      sum_cap100k_new_cases    = sum(all_new_cases / pop_100k   , na.rm = TRUE),
+      avg_cap100k_new_deaths   = mean(all_new_deaths / pop_100k , na.rm = TRUE),
+      sum_cap100k_new_deaths   = sum(all_new_deaths / pop_100k  , na.rm = TRUE),
+      avg_cap100k_new_tests    = mean(all_new_tests / pop_100k  , na.rm = TRUE),
+      sum_cap100k_new_tests    = sum(all_new_tests / pop_100k   , na.rm = TRUE),
+      continent = continent[1],
+      income = income[1],
+      who_region = who_region[1],
       .groups = "drop"
     )
-  data_table
+
+  if (grouped) {
+    data_summarized <-
+      data_summarized |>
+      rename_with(function(x) "group", {{ category }}) |>
+      filter(!is.na(group))|>
+      group_by(group) |>
+      summarize(
+        unit = group[1],
+        name = group[1],
+        pop_100k               = sum(pop_100k,  na.rm = TRUE),
+        pop                    = sum(pop, na.rm = TRUE),
+        avg_cap_new_cases      = median(avg_cap_new_cases       , na.rm = TRUE),
+        sum_cap_new_cases      = median(sum_cap_new_cases       , na.rm = TRUE),
+        avg_cap_new_deaths     = median(avg_cap_new_deaths      , na.rm = TRUE),
+        sum_cap_new_deaths     = median(sum_cap_new_deaths      , na.rm = TRUE),
+        avg_cap_new_tests      = median(avg_cap_new_tests       , na.rm = TRUE),
+        sum_cap_new_tests      = median(sum_cap_new_tests       , na.rm = TRUE),
+        avg_all_new_cases      = median(avg_all_new_cases       , na.rm = TRUE),
+        sum_all_new_cases      = sum(sum_all_new_cases          , na.rm = TRUE),
+        avg_all_new_deaths     = median(avg_all_new_deaths      , na.rm = TRUE),
+        sum_all_new_deaths     = sum(sum_all_new_deaths         , na.rm = TRUE),
+        avg_all_new_tests      = median(avg_all_new_tests       , na.rm = TRUE),
+        sum_all_new_tests      = sum(sum_all_new_tests          , na.rm = TRUE),
+        avg_pos                = median(avg_pos                 , na.rm = TRUE),
+        avg_cap100k_new_cases  = median(avg_cap100k_new_cases   , na.rm = TRUE),
+        sum_cap100k_new_cases  = median(sum_cap100k_new_cases   , na.rm = TRUE),
+        avg_cap100k_new_deaths = median(avg_cap100k_new_deaths  , na.rm = TRUE),
+        sum_cap100k_new_deaths = median(sum_cap100k_new_deaths  , na.rm = TRUE),
+        avg_cap100k_new_tests  = median(avg_cap100k_new_tests   , na.rm = TRUE),
+        sum_cap100k_new_tests  = median(sum_cap100k_new_tests   , na.rm = TRUE),
+        .groups = "drop"
+      )
+  }
+
+  data_summarized
 }
 
