a902cb9c314b44c129cdd398164e09fc36a45581
Christoph Sax <christoph.sax@gmail.com>
2022-03-27 08:21:33 +0200

    fix

--- R/get_data.R
+++ R/get_data.R
@@ -3,9 +3,18 @@
 #' Memoised Function, updates once a day
 #'
 #' @name get_data
-get_data_ <- function(dataset, version = "main", repo = "FINDtestdirData", time = today_at_sunrise()) {
+get_data_ <- function(dataset, version = c("main", "preview"), repo = "FINDtestdirData", time = today_at_sunrise()) {
+
+  version <- match.arg(version)
+
+  branch <- if (version == "main") {
+    version
+  } else {
+    paste0(version, "-", dataset)
+  }
+
   url <- paste0(
-    "https://raw.githubusercontent.com/dsbbfinddx/", repo, "/", version, "-", dataset, "/data/", dataset, ".csv"
+    "https://raw.githubusercontent.com/dsbbfinddx/", repo, "/", branch, "/data/", dataset, ".csv"
   )
   readr::read_csv(url, show_col_types = FALSE)
 }
