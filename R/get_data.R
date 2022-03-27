4dfc903fac8cbb2fb5c388215f8e0a28560e2f14
Christoph Sax <christoph.sax@gmail.com>
2022-03-27 08:18:12 +0200

    use version, not branch

--- R/get_data.R
+++ R/get_data.R
@@ -3,9 +3,9 @@
 #' Memoised Function, updates once a day
 #'
 #' @name get_data
-get_data_ <- function(dataset, repo = "FINDtestdirData", branch = "main", time = today_at_sunrise()) {
+get_data_ <- function(dataset, version = "main", repo = "FINDtestdirData", time = today_at_sunrise()) {
   url <- paste0(
-    "https://raw.githubusercontent.com/dsbbfinddx/", repo, "/", branch, "/data/", dataset, ".csv"
+    "https://raw.githubusercontent.com/dsbbfinddx/", repo, "/", version, "-", dataset, "/data/", dataset, ".csv"
   )
   readr::read_csv(url, show_col_types = FALSE)
 }
