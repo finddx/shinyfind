641d5a4727a4ebed001e7e829afd7caa46e796ea
benubah <ben@rpowerlabs.org>
2022-08-17 08:48:48 +0100

    dat -> meta_data

--- R/meta_testdir.R
+++ R/meta_testdir.R
@@ -1,16 +1,16 @@
 # Function to retrieve meta data (column definitions) for test directory shiny apps
 #' @export
-meta_cols <- function(data) {
-  meta_vars <- shinyfind::get_data(data)
+meta_cols <- function(meta_data) {
+  meta_vars <- shinyfind::get_data(meta_data)
   meta_vars
 }
 
 # Function that returns a vector of columns that can be filtered 
 # via the pickergroup
 #' @export
-meta_cols_filtered <- function(data) {
+meta_cols_filtered <- function(meta_data) {
   filtered_cols <-
-    meta_cols(data) |>
+    meta_cols(meta_data) |>
     filter(filterable == TRUE)
   
   filtered_cols
@@ -18,9 +18,9 @@ meta_cols_filtered <- function(data) {
 
 # Function that maps a column id to its full description
 #' @export
-map_id <- function(id, data) {
+map_id <- function(id, meta_data) {
   desc <- 
-    meta_cols(data) |>
+    meta_cols(meta_data) |>
     filter(id == !! id) |>
     pull(description)
   
