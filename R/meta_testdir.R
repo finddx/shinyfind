540d10f012f8b583deb9e3a8ac95e81550888041
benubah <ben@rpowerlabs.org>
2022-10-02 22:30:04 +0100

    update meta to use new folder structure

--- R/meta_testdir.R
+++ R/meta_testdir.R
@@ -1,16 +1,16 @@
 # Function to retrieve meta data (column definitions) for test directory shiny apps
 #' @export
-meta_cols <- function(meta_data) {
-  meta_vars <- shinyfind::get_data(meta_data)
+meta_cols <- function(meta_data, app) {
+  meta_vars <- shinyfind::get_data(meta_data, app)
   meta_vars
 }
 
 # Function that returns a vector of columns that can be filtered 
 # via the pickergroup
 #' @export
-meta_cols_filtered <- function(meta_data) {
+meta_cols_filtered <- function(meta_data, app) {
   filtered_cols <-
-    meta_cols(meta_data) |>
+    meta_cols(meta_data, app) |>
     filter(filterable == TRUE)
   
   filtered_cols
@@ -18,9 +18,9 @@ meta_cols_filtered <- function(meta_data) {
 
 # Function that maps a column id to its full description
 #' @export
-map_id <- function(id, meta_data) {
+map_id <- function(id, meta_data, app) {
   desc <- 
-    meta_cols(meta_data) |>
+    meta_cols(meta_data, app) |>
     filter(id == !! id) |>
     pull(description)
   
